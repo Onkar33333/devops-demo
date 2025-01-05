4cc3f49c89b5539820303e8d02f2d79f04b4fab9
Onkar33333 <onkargaykar33@gmail.com>
2025-01-05 22:50:23 +0530

    checkedr pre commit demo.py file

--- demo.py
+++ demo.py
@@ -6,7 +6,6 @@ aws_access_key = "AKIAIOSFODNN7EXAMPLE"
 # Google API Key (should be detected by detect-secrets)
 google_api_key = "AIzaSyA1234567890abcdefghijklmnopqrstuvwxyz"
 
-# Stripe API Key (should be detected by detect-secrets)
 stripe_api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
 
 # GitHub Token (should be detected by detect-secrets)
