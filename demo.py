6cae328032a51548ac9e819bea31c24bdb05e9db
Onkar33333 <onkargaykar33@gmail.com>
2025-01-05 22:51:23 +0530

    checkedr pre commit demo.py file

--- demo.py
+++ demo.py
@@ -1,17 +1,9 @@
-# Sample code that contains sensitive information
-
-# AWS Access Key (should be detected by detect-secrets)
-aws_access_key = "AKIAIOSFODNN7EXAMPLE"
-
-# Google API Key (should be detected by detect-secrets)
-google_api_key = "AIzaSyA1234567890abcdefghijklmnopqrstuvwxyz"
-
-stripe_api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
-
-# GitHub Token (should be detected by detect-secrets)
-github_token = "ghp_16H8W1sQs8KYJ0JKtUP0tz5Aa9mZp6OlUqLw"
-
-# A hardcoded password (should be detected by detect-secrets)
-password = "my$ecretP@ssw0rd123"
+def mf():
+    a = 1
+    b = 2
+    api_key = "mysecretapikey12345"  # This would be flagged by detect-secrets
 
 
+    return d
+
+mf()
