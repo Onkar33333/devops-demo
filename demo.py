5cd7a4d09050fc7d69e977326969ca0107dbee04
Onkar33333 <onkargaykar33@gmail.com>
2025-01-05 22:44:21 +0530

    checkedr pre commit demo.py file

--- demo.py
+++ demo.py
@@ -6,3 +6,13 @@ aws_access_key = "AKIAIOSFODNN7EXAMPLE"
 # Google API Key (should be detected by detect-secrets)
 google_api_key = "AIzaSyA1234567890abcdefghijklmnopqrstuvwxyz"
 
+# Stripe API Key (should be detected by detect-secrets)
+stripe_api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+
+# GitHub Token (should be detected by detect-secrets)
+github_token = "ghp_16H8W1sQs8KYJ0JKtUP0tz5Aa9mZp6OlUqLw"
+
+# A hardcoded password (should be detected by detect-secrets)
+password = "my$ecretP@ssw0rd123"
+
+
