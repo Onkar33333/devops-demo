e5ad00f53d008dc8543dc8f25b7226ef62fdc41c
Onkar33333 <onkargaykar33@gmail.com>
2025-01-05 22:33:47 +0530

    checked for pre commit demo.py file

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 def mf():
     a = 1
     b = 2
-    api_key = "mysecretapikey12345"  # This would be flagged by detect-secrets
+    api_key = "mysecretapikey1245"  # This would be flagged by detect-secrets
 
 
     return d
