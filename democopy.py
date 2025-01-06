a6f94c2f8fccb05d97e7993cddc5709d6367f843
Onkar Gaykar <96696470+Onkar33333@users.noreply.github.com>
2025-01-06 08:20:39 +0530

    Update democopy.py

--- democopy.py
+++ democopy.py
@@ -1,2 +1,9 @@
-print("hello bhai")
+"""
+This module demonstrates XYZ functionality.
+It contains basic examples of ...
+"""
+
+# Rest of your code
+
+print("ok")print("hello bhai")
 print("hello bhai again ")
